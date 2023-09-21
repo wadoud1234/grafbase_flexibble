d5014ef5418acd3186526fed8ff6acab5d1c76c6
wadoud1234 <91909315+wadoud1234@users.noreply.github.com>
2023-09-21 23:55:35 +0100

    Update page.tsx

--- src/app/create-project/page.tsx
+++ src/app/create-project/page.tsx
@@ -2,16 +2,17 @@ import Modal from "@/components/Modal"
 import ProjectForm from "@/components/ProjectForm"
 import getCurrentUser from "../../../actions/getCurrentUser"
 import { redirect } from "next/navigation"
+import { getUser } from "@/libs/graphql/query"
 
 const CreateProject = async () => {
     const session = await getCurrentUser()
-    console.log({session})
-    if (!session.user) redirect("/")
+    if (!session.user || !session.user.email) redirect("/")
+    const user = await getUser(session.user.email)
 
     return (
         <Modal>
             <h3 className="modal-head-text">Create a new project</h3>
-            <ProjectForm type="create" session={session} />
+            <ProjectForm type="create" user={user} />
         </Modal>
     )
 }
